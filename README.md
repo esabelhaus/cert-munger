a871306a1084f9712fe666abe0c3ae652632ff4a
Eric Sabelhaus <eric.sabelhaus@gmail.com>
2015-01-16 15:08:23 -0500

    certificate munging rocks, 100% coverage, version bump

--- README.md
+++ README.md
@@ -1,4 +1,40 @@
-cert-munger
+##X.509 Certificate Munger
 ===========
+This module takes string input for X509 certificates and attempts to parse and space the lines properly. It then returns the X509 attributes in JSON format
 
-This module takes string input for X509 certificates and attempts to reformat them into a valid readable certificate
+It does not accept buffer input, please use fs.readFile || fs.readFileSync in order to pass in .crt file as string.
+
+## usage
+```js
+
+var fs = require('fs'),
+      cert-munger = require('cert-munger'),
+      myCert = fs.readFileSync('path/to/cert');
+
+cert-munger.to_cert(myCert, function(cert, err){
+  console.log(cert);
+});
+
+```
+
+##Contributing
+
+
+
+###Fork it, write your code, test early, test often.
+```
+npm install -g grunt grunt-cli mocha
+npm install
+```
+
+### code of conduct
+I use a combination of Mocha and Chai in conjunction with Grunt. I also use istanbul to validate that 100% code coverage exists before merging a pull request.
+
+### testing
+`grunt`
+
+###coverage
+`istanbul cover _mocha test/test-cert-munger.js`
+
+# issues
+https://github.com/e-sabelhaus/prestige/issues
--- test/test-cert-munger.js
+++ test/test-cert-munger.js
@@ -1,6 +1,7 @@
 var assert = require('assert'),
       chai = require('chai'),
       fs = require('fs'),
+      util = require('util'),
       expect = chai.expect,
       should = chai.should(),
       certMunger = require('../lib/cert-munger.js'),
@@ -8,10 +9,12 @@ var assert = require('assert'),
       passenger = fs.readFileSync('test/certs/passenger.crt', 'utf8'),
       malformed = fs.readFileSync('test/certs/malformed.crt', 'utf8'),
       user_crt = fs.readFileSync('test/certs/node_user.crt', 'utf8');
+      tmp_crt = user_crt.toString('binary');
+      non_string_crt = new Buffer(tmp_crt, "binary");
+
 
 (function(){
   "use strict";
-
 
   var pristine = {
     "version": 2,
@@ -20,26 +23,25 @@ var assert = require('assert'),
     "commonName": 'Ruby certificate rbcert' },
     "issuer": { "domainComponent": 'ruby-lang', "commonName": 'Ruby CA' },
     "serial": '02',
-    "notBefore": '[Date: 2014-10-21T14:15:44.000Z]',
-    "notAfter": '[Date: 2015-10-21T14:15:44.000Z]',
+    "notBefore": new Date('2014-10-21T14:15:44.000Z'),
+    "notAfter": new Date('2015-10-21T14:15:44.000Z'),
     "signatureAlgorithm": 'sha256WithRSAEncryption',
     "fingerPrint": '69:81:C8:86:59:CE:A6:0B:D6:7B:16:41:C8:71:4B:82:C2:C9:BD:F4',
     "publicKey":
     { "algorithm": 'rsaEncryption',
     "e": '65537',
     "n": 'C0D826F5428371EE0849BFFABBCB302BF3DCF241B3ABEF024DF97F7FA9F453662EE8DF5251ACEDB5E1514041E2DD7F62277C7FF219C304FFA7519B463DA6B88E4A13E4453ADEAF10FC5DFEDD7FFBBF4767145BA59C414B817C51E9F1FD45718BB0C6C5F68053696E8BA445F592808BE45C6F81025FC74948CFCA38846184E4DA792339498539B03C8A9EF12769FE6721E5E12820BC2F82F1098342DDDD06C6770B6068BED27A61044C2636845C65A5D9FC497E09C9F786AAFCCDD33F9394BE7C0387C309AB82EE3CD3DCD8F04002FAD0D12154699C65C6DF5C5AD9C41A14FA8DE8C15AB8A4F01364606AD4F4722961E8386BD8B91910D9EACFF1959E17FA98E5' },
-    "altNames": [],
-    "extensions":
-    { "keyUsage": 'Digital Signature',
-    "subjectKeyIdentifier": '3C:65:4A:83:16:D4:0A:5F:CE:88:F1:E7:CD:E3:77:A9:1E:41:31:EE' }
+    "altNames": []
   };
 
 
-  describe('MUNGER:TEST: one line cert', function() {
-    it('does something', function(done){
+  describe('MUNGER:TEST: One Line Cert', function() {
+    it('Verifies certificate is munged from a single line to x509 certificate format', function(done){
       certMunger.to_cert(one_line, function(x509, error){
         if(!error) {
-          //x509.should.equal(pristine);
+          var validator = util.inspect(pristine);
+          var validatee = util.inspect(x509)
+          validatee.should.equal(validator);
           done();
         } else {
           done(error);
@@ -48,11 +50,13 @@ var assert = require('assert'),
     });
   });
 
-  describe('MUNGER:TEST: ', function() {
-    it('does something', function(done){
+  describe('MUNGER:TEST: Passenger Cert', function() {
+    it('Verifies certificate is munged from a passenger cert to x509 certificate format', function(done){
       certMunger.to_cert(passenger, function(x509, error){
         if(!error) {
-          //x509.should.equal(pristine);
+          var validator = util.inspect(pristine);
+          var validatee = util.inspect(x509)
+          validatee.should.equal(validator);
           done();
         } else {
           done(error);
@@ -61,11 +65,13 @@ var assert = require('assert'),
     });
   });
 
-  describe('MUNGER:TEST: ', function() {
-    it('does something', function(done){
+  describe('MUNGER:TEST: Malformed Cert', function() {
+    it('Verifies certificate is munged from a malformed format single line to x509 certificate format', function(done){
       certMunger.to_cert(malformed, function(x509, error){
         if(!error) {
-          //x509.should.equal(pristine);
+          var validator = util.inspect(pristine);
+          var validatee = util.inspect(x509)
+          validatee.should.equal(validator);
           done();
         } else {
           done(error);
@@ -74,14 +80,28 @@ var assert = require('assert'),
     });
   });
 
-  describe('MUNGER:TEST: ', function() {
-    it('does something', function(done){
+  describe('MUNGER:TEST: User Cert', function() {
+    it('Verifies certificate is munged from a valid x509 certtificate back to x509 certificate format', function(done){
       certMunger.to_cert(user_crt, function(x509, error){
         if(!error) {
-          //x509.should.equal(pristine);
+          var validator = util.inspect(pristine);
+          var validatee = util.inspect(x509)
+          validatee.should.equal(validator);
           done();
         } else {
           done(error);
+        }
+      })
+    });
+  });
+
+  describe('MUNGER:TEST: Buffer Of Cert', function() {
+    it('Verifies certificate cannot be passed in as a non string', function(done){
+      certMunger.to_cert(non_string_crt, function(x509, error){
+        if(!error) {
+          done("THIS IS NOT SUPPOSED TO HAPPEN!");
+        } else {
+          done();
         }
       })
     });
